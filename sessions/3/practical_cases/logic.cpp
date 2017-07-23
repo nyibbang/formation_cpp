252d2dd6d06468c71159080202c22899fcba1b1e
nyibbang <vincent.palancher@gmail.com>
2017-07-23 21:09:00 +0200

    last modifications

--- sessions/3/practical_cases/logic.cpp
+++ sessions/3/practical_cases/logic.cpp
@@ -14,17 +14,41 @@ Slot playable_column_slot(const Board& board, int column) {
     return {};
 }
 
+Player& switch_player(Player& player) {
+    switch (player) {
+        case Player::None:
+        case Player::Orange:
+            player = Player::Red;
+            break;
+        case Player::Red:
+            player = Player::Orange;
+            break;
+    }
+    return player;
+}
+
 // *************************************************************
 // TODO
 // *************************************************************
-Player& switch_player(Player& player) {
-    throw std::exception();
-}
-
 Player winner(const Board& board) {
     throw std::exception();
 }
 
 Board& play_turn(Board& board, Player player, int column) {
-    throw std::exception();
+    const auto slot = playable_column_slot(board, column);
+    if (slot.row != -1 && slot.column != -1) {
+        const auto index = slot.row * Board::column_count::value + slot.column;
+        board.value[index] = player;
+    }
+}
+
+int getColumnChoice() {
+    std::string input;
+    int choice;
+    do {
+        std::cout << "Please choose a column to play at: ";
+        std::getline(std::cin, input);
+        std::stringstream ss(input);
+        ss >> choice;
+    } while (choice <= 0 || choice > Board::column_count::value);
 }
--- sessions/3/practical_cases/tests/test.cpp
+++ sessions/3/practical_cases/tests/test.cpp
@@ -1,6 +1,18 @@
 #include "types.hpp"
 #include "ranges.hpp"
+#include "logic.hpp"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class MockFunctions : public ::testing::Mock
+{
+public:
+    MOCK_METHOD1(switch_player, Player&(Player&));
+    MOCK_METHOD1(winner, Player(const Board&));
+    MOCK_METHOD0(getColumnChoice, int());
+    MOCK_METHOD3(play_turn, Board&(Board&, Player, int));
+};
+
 
 class TestBoard : public ::testing::Test {
 protected:
@@ -96,17 +108,103 @@ TEST_F(TestBoard, RightDiagonalRange) {
     EXPECT_EQ(Player::Orange, *iter++);
 }
 
+using namespace ::testing;
+
 // ******************************************************************************
 // LOGIC
 // ******************************************************************************
 TEST(TestRun, Run) {
+    StrictMock<MockFunctions> func;
+
+    Player player;
+    Board board;
+
+    InSequence s;
+    EXPECT_CALL(func, winner(_)).WillOnce(Return(Player::None));
+    EXPECT_CALL(func, getColumnChoice()).WillOnce(Return(42));
+    EXPECT_CALL(func, play_turn(_, _, 42)).WillOnce(ReturnRef(board));
+    EXPECT_CALL(func, switch_player(_)).WillOnce(DoAll(Assign(&player, Player::Red), ReturnRef(player)));
+    EXPECT_CALL(func, winner(_)).WillOnce(Return(Player::None));
+    EXPECT_CALL(func, getColumnChoice()).WillOnce(Return(42));
+    EXPECT_CALL(func, play_turn(_, Player::Red, 42)).WillOnce(ReturnRef(board));
+    EXPECT_CALL(func, switch_player(_)).WillOnce(DoAll(Assign(&player, Player::Red), ReturnRef(player)));
+    EXPECT_CALL(func, winner(_)).WillOnce(Return(Player::Red));
+    using namespace std::placeholders;
+    run(std::bind(&MockFunctions::winner, &func, _1),
+            std::bind(&MockFunctions::play_turn, &func, _1, _2, _3),
+            std::bind(&MockFunctions::switch_player, &func, _1),
+            std::bind(&MockFunctions::getColumnChoice, &func));
 }
 
-TEST(TestSwitchPlayer, SwitchPlayer) {
+TEST(TestSwitchPlayer, SwitchPlayerOrange) {
+    Player player = Player::Orange;
+    switch_player(player);
+    EXPECT_EQ(Player::Red, player);
+}
+
+TEST(TestSwitchPlayer, SwitchPlayerRed) {
+    Player player = Player::Red;
+    switch_player(player);
+    EXPECT_EQ(Player::Orange, player);
+}
+
+TEST(TestSwitchPlayer, SwitchPlayerNone) {
+    Player player = Player::None;
+    switch_player(player);
+    EXPECT_EQ(Player::Red, player);
 }
 
 TEST(TestWinner, Winner) {
 }
 
 TEST(TestPlayTurn, PlayTurn) {
+    Board board {{{Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+
+            Player::Red,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+
+            Player::Orange,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+
+            Player::Red,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None,
+            Player::None}}};
+    play_turn(board, Player::Red, 0);
+    EXPECT_EQ(Player::Red, board.value[14]);
 }
