cf5a40f27468f545cb6ef573c8df9aad8d36ff91
nyibbang <vincent.palancher@gmail.com>
2018-10-15 20:11:49 +0200

    session 2: practical solution

--- sessions/genericcpp/2/practical/practical.cpp
+++ sessions/genericcpp/2/practical/practical.cpp
@@ -50,9 +50,38 @@ future<int> producer(Shared_data& shData)
 // queue de Shared_data avec 4 threads en s'assurant de ne pas
 // créer de data race. Pour cela, vous pouvez utiliser les outils de la STL
 // que vous souhaitez.
+future<int> consumer(Shared_data& shData)
+{
+  return async(launch::async, [&] {
+    int count = 0;
+    bool keepGoing = !shData.producingDone;
+    while (keepGoing)
+    {
+      unique_lock<mutex> lock{shData.mut};
+      if (shData.cond_var.wait_for(lock, chrono::microseconds{500}, [&]{ return !shData.data.empty(); }))
+      {
+        const auto val = shData.data.front();
+        shData.data.pop();
+        if (val % 42 == 0)
+        {
+          ++count;
+        }
+      }
+      keepGoing = !shData.producingDone || !shData.data.empty();
+    }
+    return count;
+  });
+}
+
 future<int> consumer_multithreaded(Shared_data& shData)
 {
-  throw "TODO";
+  return async(launch::async, [&] {
+    auto c1 = consumer(shData);
+    auto c2 = consumer(shData);
+    auto c3 = consumer(shData);
+    auto c4 = consumer(shData);
+    return c1.get() + c2.get() + c3.get() + c4.get();
+  });
 }
 
 int main()
